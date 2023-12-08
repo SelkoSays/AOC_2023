aba976a26178eec5f6902fafb2b344122f514ddd
SelkoSays <70065171+SelkoSays@users.noreply.github.com>
2023-12-08 11:50:31 +0100

    day 8

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -17,46 +17,21 @@ fn p1(ins: &Instrctions, map: &Map) -> usize {
 }
 
 fn p2(ins: &Instrctions, map: &Map) -> usize {
-    let mut it = 0;
-    let mut pos: Vec<String> = map
+    let pos: Vec<String> = map
         .map
         .keys()
         .filter(|k| k.ends_with('A'))
         .cloned()
         .collect();
-    let mut hm: HashMap<String, String> = HashMap::new();
-    // while !pos.iter().all(|p| p.ends_with('Z')) {
-    //     for po in pos.iter_mut() {
-    //         if let Some(p) = hm.get(po) {
-    //             *po = p.clone();
-    //             //println!("Hit: {po}");
-    //             continue;
-    //         } else {
-    //             //println!("Not hit: {po}");
-    //             let mut cpo = po.clone();
-    //             for i in &ins.0 {
-    //                 if let Some(p) = map.map.get(&cpo) {
-    //                     cpo = p[*i as usize].clone();
-    //                 }
-    //             }
-    //             hm.insert(po.clone(), cpo.clone());
-    //             *po = cpo;
-    //         }
-    //     }
-    //     it += ins.0.len();
+    let cy: Vec<usize> = pos
+        .iter()
+        .map(|s| map.calc_cycle(s, ins))
+        .map(|(x, _)| x)
+        .collect();
+    cy.iter().fold(1, |acc, v| lcm(acc, *v))
+    // for k in &pos {
+    //     println!("key: {k}, {:?}", map.calc_cycle(k, ins));
     // }
-    for i in ins.0.iter().cycle() {
-        if pos.iter().all(|p| p.ends_with('Z')) {
-            break;
-        }
-        for po in pos.iter_mut() {
-            if let Some(p) = map.map.get(po) {
-                *po = p[*i as usize].clone();
-            }
-        }
-        it += 1;
-    }
-    it
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -102,6 +77,27 @@ impl Map {
             map: v.into_iter().map(|n| (n.0, n.1)).collect(),
         }
     }
+
+    pub fn calc_cycle(&self, start: &str, ins: &Instrctions) -> (usize, usize) {
+        let mut it = 0;
+        let mut pos = start;
+        let mut it_to_z = 0;
+        let mut passed_z = 1;
+        for i in ins.0.iter().cycle() {
+            it += 1 ^ passed_z;
+            it_to_z += 1 & passed_z;
+            if let Some(p) = self.map.get(pos) {
+                pos = &p[*i as usize];
+            }
+            if pos.ends_with('Z') {
+                if passed_z == 0 {
+                    break;
+                }
+                passed_z = 0;
+            }
+        }
+        (it, it_to_z)
+    }
 }
 
 struct N(String, [String; 2]);
@@ -118,9 +114,32 @@ impl FromStr for N {
     }
 }
 
+fn lcm(first: usize, second: usize) -> usize {
+    first * second / gcd(first, second)
+}
+
+fn gcd(first: usize, second: usize) -> usize {
+    let mut max = first;
+    let mut min = second;
+    if min > max {
+        std::mem::swap(&mut max, &mut min);
+    }
+
+    loop {
+        let res = max % min;
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
+
 fn main() {
     let (ins, v): (Instrctions, Vec<N>) = Input::default().read_headers_n_data().unwrap();
     let data = Map::from_vec_n(v);
+    //p2(&ins, &data);
     println!("P1: {}", p1(&ins, &data));
     println!("P2: {}", p2(&ins, &data));
 }
@@ -186,10 +205,19 @@ XXX = (XXX, XXX)",
     .read_headers_n_data()
     .unwrap();
     let data = Map::from_vec_n(v);
+    assert_eq!(data.calc_cycle("11A", &ins), (2, 2));
+    assert_eq!(data.calc_cycle("22A", &ins), (3, 3));
     assert_eq!(6, p2(&ins, &data));
 }
 
 #[test]
 fn final_test() {
-    let input = Input::file("./data/day8.txt").unwrap().lines();
+    let (ins, v): (Instrctions, Vec<N>) = Input::file("./data/day8.txt")
+        .unwrap()
+        .read_headers_n_data()
+        .unwrap();
+    let data = Map::from_vec_n(v);
+    //p2(&ins, &data);
+    assert_eq!(20777, p1(&ins, &data));
+    assert_eq!(13289612809129, p2(&ins, &data));
 }
