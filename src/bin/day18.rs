b749760821bbfecf4476a619b08f2afd81060352
SelkoSays <70065171+SelkoSays@users.noreply.github.com>
2023-12-18 13:32:46 +0100

    day18

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,7 +1,6 @@
 use std::str::FromStr;
 
 use aoc_2023::input::Input;
-use aoc_2023::matrix::Matrix;
 
 fn p1(data: &[Dig]) -> usize {
     let mut points: Vec<(isize, isize)> = Vec::new();
@@ -10,124 +9,41 @@ fn p1(data: &[Dig]) -> usize {
     for d in data {
         points.push((x, y));
         match d.dir {
-            Dir::Up => y -= d.amm as isize,
-            Dir::Down => y += d.amm as isize,
+            Dir::Up => y += d.amm as isize,
+            Dir::Down => y -= d.amm as isize,
             Dir::Left => x -= d.amm as isize,
             Dir::Right => x += d.amm as isize,
         }
     }
-    let (vx, vy): (Vec<isize>, Vec<isize>) = points.iter().cloned().unzip();
-    let mx = vx.iter().min().unwrap();
-    let my = vy.iter().min().unwrap();
-    let lx = vx.iter().max().unwrap() - mx;
-    let ly = vy.iter().max().unwrap() - my;
-    let mut table = Matrix(vec![vec![Ign::Empty; lx as usize]; ly as usize]);
-    let x_off = mx.abs();
-    let y_off = my.abs();
-    for (i, p) in points.iter().enumerate() {
-        let n = (i + 1) % points.len();
-        if let Some(e) = table.get_mut((p.0 + x_off) as usize, (p.1 + y_off) as usize) {
-            *e = Ign::Pending;
-            if points[n].0 != p.0 {
-                let y = (p.1 + y_off) as usize;
-                for x in p.0.min(points[n].0)..p.0.max(points[n].0) {
-                    if let Some(e) = table.get_mut((x + x_off) as usize, y) {
-                        *e = Ign::Pending;
-                    }
-                }
-            } else if points[n].1 != p.1 {
-                let x = (p.0 + x_off) as usize;
-                for y in p.0.min(points[n].1)..p.0.max(points[n].1) {
-                    if let Some(e) = table.get_mut(x, (y + y_off) as usize) {
-                        *e = Ign::Pending;
-                    }
-                }
-            }
-        }
-    }
-    for y in 0..ly as usize {
-        for x in 0..lx as usize {
-            Ign::facilitate(&mut table, (x as isize, y as isize));
-        }
-    }
-    for y in 0..ly as usize {
-        let mut y1 = y;
-        let mut in_area = false;
-        for x in 0..y {
-            if let Some(e) = table.get_mut(x, y1) {
-                match e {
-                    Ign::Empty => *e = if in_area { Ign::Inner } else { Ign::Outer },
-                    Ign::NotIgnore => in_area = !in_area,
-                    _ => {}
-                }
-            }
-            y1 -= 1;
-        }
-    }
-    println!("{table}");
-    table
-        .iter()
-        .filter(|&e| matches!(*e, Ign::Inner | Ign::NotIgnore | Ign::Ignore))
-        .count()
+    //println!(" Points: {points:?}");
+    trapezoid_formula(&points) + calc_edge(&points)
 }
 
-fn p2() -> u32 {
-    0
+fn p2(data: &[Dig]) -> usize {
+    let data: Vec<Dig> = data.iter().map(|d| d.transfonm()).collect();
+    p1(&data)
 }
 
-#[derive(Debug, Clone)]
-enum Ign {
-    Empty,
-    Ignore,
-    NotIgnore,
-    Inner,
-    Outer,
-    Pending,
-}
-
-impl std::fmt::Display for Ign {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let s = match self {
-            Ign::Empty => ".",
-            Ign::Ignore => "#",
-            Ign::NotIgnore => "#",
-            Ign::Inner => "I",
-            Ign::Outer => "O",
-            Ign::Pending => "P",
-        };
-        write!(f, "{s}")
+fn trapezoid_formula(points: &[(isize, isize)]) -> usize {
+    let mut sum = 0;
+    for i in 0..points.len() - 1 {
+        let p = points[i];
+        sum += (p.1 + points[i + 1].1) * (p.0 - points[i + 1].0);
     }
+    sum.unsigned_abs() / 2
 }
 
-type Point = (isize, isize);
-impl Ign {
-    // pub fn make(pp: Point, cp: Point, np: Point) -> Self {
-    //     if (pp.0 == cp.0 && cp.0 == np.0) || (pp.1 == cp.1 && cp.1 == np.1) {
-    //         // pp x == cp x == np x
-    //         return Self::NotIgnore;
-    //     }
-    //     if (pp.0 < cp.0 && cp.1 < np.1) || (cp.0 == np.0 && pp.1 < cp.1) {
-    //         // -+  ali |
-    //         //  |      +-
-    //         return Self::NotIgnore;
-    //     }
-    //     Self::Ignore
-    // }
-    pub fn facilitate(table: &mut Matrix<Self>, p: (isize, isize)) {
-        let up = table.get(p.0 as usize, (p.1 - 1) as usize).cloned();
-        let left = table.get((p.0 - 1) as usize, p.1 as usize).cloned();
-        let down = table.get(p.0 as usize, (p.1 + 1) as usize).cloned();
-        let right = table.get((p.0 - 1) as usize, p.1 as usize).cloned();
-        if let Some(e) = table.get_mut(p.0 as usize, p.1 as usize) {
-            if matches!(e, Self::Pending) {
-                if (up.is_some() && left.is_some()) || (down.is_some() && right.is_some()) {
-                    *e = Self::Ignore;
-                } else {
-                    *e = Self::NotIgnore;
-                }
-            }
-        }
+fn distance(p1: (isize, isize), p2: (isize, isize)) -> usize {
+    (p1.0 - p2.0).unsigned_abs() + (p1.1 - p2.1).unsigned_abs()
+}
+
+fn calc_edge(points: &[(isize, isize)]) -> usize {
+    let mut sum = 0;
+    for i in 0..points.len() - 1 {
+        sum += distance(points[i], points[i + 1]);
     }
+    sum += distance(points[0], *points.last().unwrap());
+    sum / 2 + 1
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -136,6 +52,18 @@ enum Dir {
     Down,
     Left,
     Right,
+}
+
+impl From<usize> for Dir {
+    fn from(value: usize) -> Self {
+        match value {
+            0 => Self::Right,
+            1 => Self::Down,
+            2 => Self::Left,
+            3 => Self::Up,
+            _ => panic!("Wrong value: {value}"),
+        }
+    }
 }
 
 impl From<&str> for Dir {
@@ -153,8 +81,21 @@ impl From<&str> for Dir {
 #[derive(Debug, PartialEq, Eq)]
 struct Dig {
     dir: Dir,
-    amm: u8,
+    amm: usize,
     clr: String,
+}
+
+impl Dig {
+    pub fn transfonm(&self) -> Self {
+        let n = usize::from_str_radix(&self.clr, 16).unwrap();
+        let dir = (n & 0b1111).into(); // mod 16
+        let amm = n >> 4; // div 16
+        Self {
+            dir,
+            amm,
+            clr: self.clr.clone(),
+        }
+    }
 }
 
 impl FromStr for Dig {
@@ -165,7 +106,7 @@ impl FromStr for Dig {
         let dir: Dir = d.into();
         let (a, c) = r.split_once(' ').unwrap();
         let amm = a.parse().map_err(|_| ())?;
-        let clr = c.replace(['(', ')'], "");
+        let clr = c.replace(['(', ')', '#'], "");
         Ok(Self { dir, amm, clr })
     }
 }
@@ -173,7 +114,7 @@ impl FromStr for Dig {
 fn main() {
     let input: Vec<Dig> = Input::default().read_data().unwrap();
     println!("P1: {}", p1(&input));
-    println!("P2: {}", p2());
+    println!("P2: {}", p2(&input));
 }
 
 #[test]
@@ -204,7 +145,7 @@ U 2 (#7a21e3)
         Dig {
             dir: Dir::Right,
             amm: 6,
-            clr: "#70c710".into()
+            clr: "70c710".into()
         }
     );
     assert_eq!(p1(&input), 62);
